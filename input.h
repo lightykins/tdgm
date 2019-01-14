22d7443ff46002aadcef173198d1c42db6b50073
twig33 <43547813+twig33@users.noreply.github.com>
2019-01-14 07:47:38 -0800

    Add files via upload

---
+++ input.h
@@ -0,0 +1,1 @@
+const std::string hello = "hello world";
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <input.h>
+
+int main()
+{
+    std::cout << hello << "\n";
+	std::cout << "Press enter\n";
+	std::cin.get();
+    return 0;
+}

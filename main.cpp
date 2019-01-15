d166be9a191d4963df64e67d88c67e4f8b16ac02
twig33 <43547813+twig33@users.noreply.github.com>
2019-01-15 08:10:30 -0800

    Output colors

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <windows.h>
 #include <input.h>
 #include "SDL.h"
+enum color {blue = 1, green, cyan, red, purple, dyellow, white, gray, bblue, bcyan, bred, pink, yellow, bwhite};
+void setColor(int color = white){
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
 int main(int argc, char* argv[]){
-    SDL_Init(SDL_INIT_VIDEO);
-	SDL_Quit();
+    if (SDL_Init(SDL_INIT_VIDEO) != 0){
+	    std::cout << SDL_GetError() << "\n";
+	}
+	setColor(green);
 	std::cout << hello + "\n";
+	setColor();
 	std::cin.get(); 
+	SDL_Quit();
 	return 0;
 }
